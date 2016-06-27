892f48bc71b67eeeb1ceef77e085a6266a84b98e
Niraj Yadav <nirajy@microsoft.com>
2016-06-27 15:40:33 -0400

    Adding Data Grouping and User Input R scripts

--- R-scripts/Data_Modification.R
+++ R-scripts/Data_Modification.R
@@ -1,6 +1,4 @@
-
 #Read Json Data
-
 library(jsonlite)
 inputData <- fromJSON("output10json.txt")
 
@@ -16,31 +14,8 @@ for (words in inputData$Regions$Lines[[1]]$Words)
        for (len in 1:l)
        {
                newRow <- data.frame(Text = words$Text[len],Position_top = words$Rectangle$Top[len] , Position_left= words$Rectangle$Left[len] , Size_height = words$Rectangle$Height[len], Size_width = words$Rectangle$Width[len], Label = "", Line_No = Line_number )
-               
-               
-               #Check Email Address Format
-               if(length(grep("(@)",newRow$Text)) > 0 )
-               {
-                       newRow$Label <- "Email Address"
-               }
-               if(length(grep("^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$",newRow$Text)) > 0)
-               {
-                       newRow$Label <- "Phone Number" 
-               }
-                       
                outputdata <- rbind(outputdata,data.frame(newRow))
        }
         Line_number <- Line_number + 1
 }
-#Base Region top left corner to 0,0 and adjust each word position accordingly
-outputdata$Position_top <- outputdata$Position_top - inputData$Regions$Rectangle$Top
-outputdata$Position_left <- outputdata$Position_left - inputData$Regions$Rectangle$Left
-
-#Scale Height and Width to 100:60
-outputdata$Size_height <- outputdata$Size_height / (inputData$Regions$Rectangle$Height/60)
-outputdata$Size_width <- outputdata$Size_width / (inputData$Regions$Rectangle$Width/100)
-
-outputdata$Position_top <- outputdata$Position_top / (inputData$Regions$Rectangle$Height/60)
-outputdata$Position_left <- outputdata$Position_left / (inputData$Regions$Rectangle$Width/100)
-
 
