db581e4e4085b24da045004f17e4ebcfa43af13a
Alfredo Cerritos <alcerri@microsoft.com>
2016-07-14 13:22:33 -0400

    Adding R Dot Net to call R from C#
    Adding R Dot Net to call R from C#

---
+++ RNET/RNET/Program.cs
@@ -0,0 +1,30 @@
+﻿using RDotNet;
+using System;
+using System.IO;
+
+namespace RNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("click to start ...");
+            Console.ReadKey();
+
+            REngine.SetEnvironmentVariables();
+            REngine engine = REngine.GetInstance();
+            engine.Initialize();
+
+
+            string text = File.ReadAllText(@"C:\dev\github\cognitivecrm\R-scripts\Data_Grouping.txt");
+            var myFunc = engine.Evaluate(text).AsFunction();
+            var v1 = engine.CreateCharacter(@"C:\dev\github\cognitivecrm\R-scripts\output10json.txt");
+            var df = myFunc.Invoke(new SymbolicExpression[] { v1 }).AsDataFrame();
+
+            Console.Write("click to finish ...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
