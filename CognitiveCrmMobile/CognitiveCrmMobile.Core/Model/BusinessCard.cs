2234135c41908daf6d831dcda82f9d830958e4ec
Alfredo Cerritos <alcerri@microsoft.com>
2016-07-26 17:03:10 -0400

    Checking in the Xamarin Project
    Any Changes

---
+++ CognitiveCrmMobile/CognitiveCrmMobile.Core/Model/BusinessCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveCrmMobile.Core.Model
+{
+    public class BusinessCard
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber1 { get; set; }
+        public string PhoneNumber2 { get; set; }
+        public string CompanyName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string EmailAddress1 { get; set; }
+        public string EmailAddress2 { get; set; }
+        public string WebSite { get; set; }
+        public string Designation { get; set; }
+        public string IgnoreText { get; set; }
+        public string NameDesignation { get; set; }
+        public string PhoneNumber1PhoneNumber2 { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
