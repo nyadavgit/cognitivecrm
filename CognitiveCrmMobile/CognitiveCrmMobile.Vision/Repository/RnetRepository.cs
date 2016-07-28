4f62c6b963e6fd09645fce88abb4d4ed7098c6a1
Alfredo Cerritos <alcerri@microsoft.com>
2016-07-28 10:11:42 -0400

    latest changes

--- CognitiveCrmMobile/CognitiveCrmMobile.Vision/Repository/RnetRepository.cs
+++ CognitiveCrmMobile/CognitiveCrmMobile.Vision/Repository/RnetRepository.cs
@@ -16,12 +16,12 @@ namespace CognitativeCrmVision.Repository
             var scriptText = File.ReadAllText(scriptFile);
             var myFunc = engine.Evaluate(scriptText).AsFunction();
             //var v1 = engine.CreateCharacter(filepath);
-            var jsonTestFile = @"C:\Users\alcerri.NORTHAMERICA\Documents\GitHub\cognitivecrm\cognitivecrm\output10json.txt";
+            //var jsonTestFile = @"C:\Users\alcerri.NORTHAMERICA\Documents\GitHub\cognitivecrm\cognitivecrm\output10json.txt";
             //var v1 = engine.CreateCharacter(jsonTest);
 
-           var sampleText = File.ReadAllText(jsonTestFile);
-            var v1 = engine.CreateCharacter(sampleText.ToString());
-            // var v1 = engine.CreateCharacter(jsonResponse);
+           //var sampleText = File.ReadAllText(jsonTestFile);
+           // var v1 = engine.CreateCharacter(sampleText);
+            var v1 = engine.CreateCharacter(jsonResponse);
             var df = myFunc.Invoke(new SymbolicExpression[] { v1 }).AsDataFrame();
 
             //var dt = RDataFrameToDataSet(df);
--- CognitiveCrmMobile/ConsoleApplicationTest/Program.cs
+++ CognitiveCrmMobile/ConsoleApplicationTest/Program.cs
@@ -22,6 +22,8 @@ namespace ConsoleApplicationTest
         {
             Console.WriteLine("Let's Run");
             Console.ReadKey();
+
+
             var service = new CognitativeCrmVision.Services.VisionService();
             var stream = GetStream(@"\002.jpg");
             string result = service.CognitiveResult(stream);
